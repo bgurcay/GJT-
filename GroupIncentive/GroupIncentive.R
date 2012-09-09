e46be71597a48a1c87c6ddb18877d7d92158c393
Burcu Gurcay <bgurcay@sas.upenn.edu>
2012-09-09 18:55:32 -0400

    Control data re-organization completed

--- GroupIncentive/GroupIncentive.R
+++ GroupIncentive/GroupIncentive.R
@@ -29,18 +29,22 @@ go.data<-read.csv("/Users/bgurcay86/Documents/Penn/Research/GJT/GroupIncentive/d
 # look at the estimations but not confidence ratings.
 
 selection <- sort(sample(1:length(control.data$labID), 100, replace=F)) #the rows (Ss) to be selected from the raw control data file 
-control.data <- control.data[selection,]
-numbers.con <- seq(1:length(names(control.data)))
-est.con <- numbers.con[seq(2,length(names(control.data)),by=2)] #the cols that have the estimations
-conf.con <- numbers.con[seq(3,length(names(control.data)),by=2)] #the cols that have the confidence ratings
-control.data.est <- control.data[,c(1,est.con)] # selects the even cols (Estimations) and the Subj ID columns and saves it as a new data file
-control.data.conf <- control.data[,c(1,conf.con)] # selects the odds cols (Confidence ratings) and the Subj ID columns and saves it as a new data file
+control.data.100 <- control.data[selection,]
+numbers.con <- seq(1:length(names(control.data.100)))
+est.con <- numbers.con[seq(2,length(names(control.data.100)),by=2)] #the cols that have the estimations
+conf.con <- numbers.con[seq(3,length(names(control.data.100)),by=2)] #the cols that have the confidence ratings
+control.data.est <- control.data.100[,c(1,est.con)] # selects the even cols (Estimations) and the Subj ID columns and saves it as a new data file
+control.data.conf <- control.data.100[,c(1,conf.con)] # selects the odds cols (Confidence ratings) and the Subj ID columns and saves it as a new data file
 
 #add a group number for each 10 subjects to form groups in the control condition
 
-control.data$group <-rep(NA,nrow(control.data))
-control.data$group <- c(rep(1,10), rep(2,10), rep(3,10), rep(4,10), rep(5,10), rep(6,10), rep(7,10), rep(8,10), rep(9,10), rep(10,10)) 
-control.data
+control.data.100$group <-rep(NA,nrow(control.data.100))
+control.data.100$group <- c(rep(1,10), rep(2,10), rep(3,10), rep(4,10), rep(5,10), rep(6,10), rep(7,10), rep(8,10), rep(9,10), rep(10,10)) 
+control.data.est$group <- c(rep(1,10), rep(2,10), rep(3,10), rep(4,10), rep(5,10), rep(6,10), rep(7,10), rep(8,10), rep(9,10), rep(10,10)) 
+control.data.conf$group <- c(rep(1,10), rep(2,10), rep(3,10), rep(4,10), rep(5,10), rep(6,10), rep(7,10), rep(8,10), rep(9,10), rep(10,10)) 
+control.data.100
+control.data.est
+control.data.conf
 
 ## calculate absolute percentage error
 
@@ -50,9 +54,9 @@ abs.percentage.error <-function(x,v) {
 
 # transform the control.data files into absolute percentage error
 
-output <- abs.percentage.error(control.data.est[,c(2:17)],true.values)
-output$group <- control.data.est$group
-output
+output.con.est <- abs.percentage.error(control.data.est[,c(2:17)],true.values)
+output.con.est$group <- control.data.est$group
+output.con.est
 
 ### IO Condition Data Manipulation ###
 
