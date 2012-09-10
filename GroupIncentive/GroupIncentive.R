a0d76707e3610d3ac6c11435db1400e46af7b188
Burcu Gurcay <bgurcay@sas.upenn.edu>
2012-09-10 13:31:34 -0400

    Abs. % error calculations are also complete for the IO condition.

--- GroupIncentive/GroupIncentive.R
+++ GroupIncentive/GroupIncentive.R
@@ -92,14 +92,53 @@ io.data.1st.est <- io.data[,c(1,2,everyfour.IO.1st.est)]
 everyfour.IO.2nd.est <- numbers.IO[seq(5,length(names(io.data)),by=4)] 
 io.data.2nd.est <- io.data[,c(1,2,everyfour.IO.2nd.est)]
 
-# transform the io.data.1st.est and io.data.2nd.est into absolute percentage error
+## transform the io.data.1st.est and io.data.2nd.est into absolute percentage error and save them in new data frames separately
 
-### NEEDS TO BE FIXED ###
+# transformation for io.data.1st.est
 
-absPercentErr.IO.1st.est <- abs.percentage.error(io.data.1st.est[,c(3:length(names(io.data.1st.est)))],true.values)
-absPercentErr.IO.2nd.est <- abs.percentage.error(io.data.2nd.est[,c(3:length(names(io.data.2nd.est)))],true.values)
-absPercentErr.IO.1st.est
-absPercentErr.IO.2nd.est
+q1.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,3],true.values[1])
+q2.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,4],true.values[2])
+q3.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,5],true.values[3])
+q4.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,6],true.values[4])
+q5.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,7],true.values[5])
+q6.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,8],true.values[6])
+q7.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,9],true.values[7])
+q8.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,10],true.values[8])
+q9.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,11],true.values[9])
+q10.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,12],true.values[10])
+q11.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,13],true.values[11])
+q12.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,14],true.values[12])
+q13.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,15],true.values[13])
+q14.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,16],true.values[14])
+q15.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,17],true.values[15])
+q16.abs.perErr.IO.est1 <- abs.percentage.error(io.data.1st.est[,18],true.values[16])
+
+abs.perErr.IO.est1 <- data.frame(labID=io.data.1st.est[,1], experiment_number=io.data.1st.est[,2], q1est1=q1.abs.perErr.IO.est1, q2est1=q2.abs.perErr.IO.est1, q3est1=q3.abs.perErr.IO.est1, q4est1=q4.abs.perErr.IO.est1, q5est1=q5.abs.perErr.IO.est1, q6est1=q6.abs.perErr.IO.est1, q7est1=q7.abs.perErr.IO.est1, q8est1=q8.abs.perErr.IO.est1, q9est1=q9.abs.perErr.IO.est1, q10est1=q10.abs.perErr.IO.est1, q11est1=q11.abs.perErr.IO.est1, q12est1=q12.abs.perErr.IO.est1, q13est1=q13.abs.perErr.IO.est1, q14est1=q14.abs.perErr.IO.est1, q15est1=q15.abs.perErr.IO.est1, q16est1=q16.abs.perErr.IO.est1)
+
+abs.perErr.IO.est1
+
+# transformation for io.data.2nd.est
+
+q1.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,3],true.values[1])
+q2.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,4],true.values[2])
+q3.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,5],true.values[3])
+q4.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,6],true.values[4])
+q5.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,7],true.values[5])
+q6.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,8],true.values[6])
+q7.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,9],true.values[7])
+q8.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,10],true.values[8])
+q9.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,11],true.values[9])
+q10.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,12],true.values[10])
+q11.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,13],true.values[11])
+q12.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,14],true.values[12])
+q13.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,15],true.values[13])
+q14.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,16],true.values[14])
+q15.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,17],true.values[15])
+q16.abs.perErr.IO.est2 <- abs.percentage.error(io.data.2nd.est[,18],true.values[16])
+
+abs.perErr.IO.est2 <- data.frame(labID=io.data.2nd.est[,1], experiment_number=io.data.2nd.est[,2], q1est2=q1.abs.perErr.IO.est2, q2est2=q2.abs.perErr.IO.est2, q3est2=q3.abs.perErr.IO.est2, q4est2=q4.abs.perErr.IO.est2, q5est2=q5.abs.perErr.IO.est2, q6est2=q6.abs.perErr.IO.est2, q7est2=q7.abs.perErr.IO.est2, q8est2=q8.abs.perErr.IO.est2, q9est2=q9.abs.perErr.IO.est2, q10est2=q10.abs.perErr.IO.est2, q11est2=q11.abs.perErr.IO.est2, q12est2=q12.abs.perErr.IO.est2, q13est2=q13.abs.perErr.IO.est2, q14est2=q14.abs.perErr.IO.est2, q15est2=q15.abs.perErr.IO.est2, q16est2=q16.abs.perErr.IO.est2)
+
+abs.perErr.IO.est2
 
 # pick only the first confidence rating cols
 
@@ -129,7 +168,7 @@ go.data.1st.est <- go.data[,c(1,2,everyfour.GO.1st.est)]
 everyfour.GO.2nd.est <- numbers.GO[seq(5,length(names(go.data)),by=4)]
 go.data.2nd.est <- go.data[,c(1,2,everyfour.GO.2nd.est)]
 
-# transform the go.data.1st.est and go.data.2nd.est into absolute percentage error
+## transform the go.data.1st.est and go.data.2nd.est into absolute percentage error and save them in new data frames separately
 
 
 
@@ -145,5 +184,5 @@ go.data.2nd.conf <- go.data[,c(1,2,everyfour.GO.2nd.conf)]
 
 # do a linear transformation on confidence ratings so that this condition matches the control condition
 
-go.data.1st.conf.tr <- ceiling(go.data.1st.conf/2)
-go.data.2nd.conf.tr <- ceiling(go.data.2nd.conf/2)
+go.data.1st.conf.tr <- ceiling(go.data.1st.conf/2) #first confidence rating transformation
+go.data.2nd.conf.tr <- ceiling(go.data.2nd.conf/2) #second confidence rating transformation
